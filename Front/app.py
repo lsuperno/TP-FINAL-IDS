d7a61f65872277c7ed32fb8b714c06a9a737041e
Tu nombre <you@example.com>
2025-10-27 02:30:09 -0300

    Template adaptado a flask
    Se agrega template adaptado a flask con la estructura de carpetas, un archivo html base.html para utilizar bloques y un app.py

---
+++ Front/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/iniciar_sesion')
+def iniciar_sesion():
+    return render_template('signin.html')
+
+@app.route('/registrar_cuenta')
+def registrar_cuenta():
+    return render_template('signup.html')
+
+@app.route('/cambiar_contraseña')
+def cambiar_contraseña():
+    return render_template('reset-password.html')
+
+@app.route('/precios')
+def precios():
+    return render_template('pricing.html')
+
+@app.route('/mail')
+def mail():
+    return render_template('mail-success.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contact.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog-single.html')
+
+@app.route('/blog_grilla')
+def blog_grilla():
+    return render_template('blog-grid.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('about-us.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run('localhost', port=8080, debug = True)
